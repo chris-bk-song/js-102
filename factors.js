8d7d825fa1ef88c66ab9ce2338695b598b9f8099
Chris Song <chris.bk.song@gmail.com>
2020-04-20 16:27:47 -0400

    made some correction

--- factors.js
+++ factors.js
@@ -2,13 +2,13 @@ function factors(num) {
   var arr = [];
   for (let count = 1; count <= num; count ++) {
     if (num % count === 0) {
-      console.log(count)
       arr.push(count);
-      console.log(arr);
     }
   }
+  console.log(arr);
 }
 
 factors(120);
 
 // how to output value from last iteration only? 
+// place console.log(arr) outside of for loop will only display last iteration
