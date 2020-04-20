a9e1238b131a903b562be7826a902672f6178ff1
Chris Song <chris.bk.song@gmail.com>
2020-04-20 16:12:20 -0400

    creates factors function that returns an array containing all its factors when a number is given

---
+++ factors.js
@@ -0,0 +1,14 @@
+function factors(num) {
+  var arr = [];
+  for (let count = 1; count <= num; count ++) {
+    if (num % count === 0) {
+      console.log(count)
+      arr.push(count);
+      console.log(arr);
+    }
+  }
+}
+
+factors(120);
+
+// how to output value from last iteration only? 
